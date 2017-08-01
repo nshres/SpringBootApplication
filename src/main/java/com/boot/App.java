4830c8395d21eed267b40d04bc87e2fbdc647e9e
nshres <shresjava@gmail.com>
2017-07-31 22:21:45 -0500

    Project Setup

---
+++ src/main/java/com/boot/App.java
@@ -0,0 +1,24 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**@author Nishan
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	SpringApplication.run(App.class, args);
+    }
+}
+
+
+		
